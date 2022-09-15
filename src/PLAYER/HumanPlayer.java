f38a93c4123c4bb9c511ab643cd3d144409c0980
DominicVerschoor <dominic.verschoor@student.maastrichtuniversity.nl>
2022-09-15 17:37:05 +0200

    Completed Player interface. Collisions on gameloop lines 20 and 31. I wanted to add the color of the player in the constructor, but color is not present in game loop. I believe its a simple change but didnt want to make things messy.

--- src/PLAYER/HumanPlayer.java
+++ src/PLAYER/HumanPlayer.java
@@ -1,4 +1,60 @@
 package PLAYER;
 
+import GAME.Cell;
+
 public class HumanPlayer implements Player{
+    private int score;
+    private int playerColor;
+    private int pieceColor;
+
+    public HumanPlayer(int playerColor){
+        this.playerColor = playerColor;
+        this.pieceColor = playerColor;
+        this.score = 0;
+    }
+
+    @Override
+    public void changeToNextPieceColor() {
+
+    }
+
+    @Override
+    public void placeCurrentPieceOnCell(Cell cell) {
+
+    }
+
+    @Override
+    public boolean turnIsOver() {
+        return false;
+    }
+
+    @Override
+    public void setPlayersColor(int playersColor) {
+        this.playerColor = playersColor;
+    }
+
+    @Override
+    public int getPlayersColor() {
+        return playerColor;
+    }
+
+    @Override
+    public void setPieceColor(int pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    @Override
+    public int getPieceColor() {
+        return pieceColor;
+    }
+
+    @Override
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
 }
