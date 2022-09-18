c9bf562d4d70d33e500666bbb1a94792e1e1c196
DominicVerschoor <dominic.verschoor@student.maastrichtuniversity.nl>
2022-09-18 22:06:22 +0200

    Temporary GUI for player

--- src/UI/PlayerDisplayInformation.java
+++ src/UI/PlayerDisplayInformation.java
@@ -12,63 +12,103 @@ public class PlayerDisplayInformation {
     /**
      * tmporary test field
      */
-    public static void createTestField(){
-        JFrame f=new JFrame();//creating instance of JFrame
-        f.setSize(width,height);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
+    public static void createTestField() {
+        JFrame f = new JFrame();
+        f.setSize(width, height);
+        f.setLayout(new GridBagLayout());
+        f.getContentPane().setBackground(new Color(238, 238, 173));
+        GridBagConstraints constraints = new GridBagConstraints();
 
-        addPlayer(f,0,0, Color.gray);
-        addPlayer(f,width-215,0, Color.red);
-        addPlayer(f,0,height-125, Color.green);
-        addPlayer(f,width-215,height-125, Color.yellow);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.gridwidth = 3;
+        constraints.gridheight = 3;
+        setConstraints(constraints, 1, 1, 0.0, 0.0);
+        f.add(fakeBoard(), constraints);
+        constraints.gridwidth = 0;
+        constraints.gridheight = 0;
+
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        setConstraints(constraints, 0, 0, 1.0, 0.0);
+        f.add(addPlayer(Color.white, "White Guy"), constraints);
+
+        constraints.anchor = GridBagConstraints.FIRST_LINE_END;
+        setConstraints(constraints, 2, 0, 0.0, 0.0);
+        f.add(addPlayer(Color.black, "Black Guy"), constraints);
+
+        constraints.anchor = GridBagConstraints.LAST_LINE_START;
+        setConstraints(constraints, 0, 2, 0.0, 0.0);
+        f.add(addPlayer(Color.red, "Red Guy"), constraints);
+
+        constraints.anchor = GridBagConstraints.LAST_LINE_END;
+        setConstraints(constraints, 2, 2, 0.0, 1.0);
+        f.add(addPlayer(Color.blue, "Blue Guy"), constraints);
 
         f.setLocationRelativeTo(null);
+        f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private static void addPlayer(JFrame frame, int x, int y, Color color){
+    private static JPanel fakeBoard() {
+        JPanel board = new JPanel();
+
+        board.setPreferredSize(new Dimension(350, 350));
+
+        board.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(5.0f), Color.MAGENTA));
+        return board;
+    }
+
+    private static JPanel addPlayer(Color playerColor, String playerName) {
         JPanel player = new JPanel(new GridBagLayout());
-        player.setBackground(color);
+        player.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(5.0f), playerColor));
+        player.setPreferredSize(new Dimension(200, 100));
+        player.setBackground(Color.lightGray);
 
-        player.setBounds(x, y, 200, 100);
+        addPlayerDetails(player, Color.white, 0, playerName);
 
-        addPlayerDetails(player);
-
-        frame.add(player);
+        return player;
     }
 
-    public static void addPlayerDetails(JPanel player){
+    public static void addPlayerDetails(JPanel player, Color pieceColor, int playerScore, String playerName) {
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.insets = new Insets(5,5,5,5);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.insets = new Insets(7, 7, 7, 7);
 
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        JLabel t1 = new JLabel("test1");
-        t1.setFont(new Font("Serif", Font.PLAIN, 20));
-        player.add(t1, constraints);
+        Font font = new Font("Serif", Font.PLAIN, 20);
 
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        constraints.gridwidth = 2;
-        JLabel t2 = new JLabel("test2");
-        t2.setFont(new Font("Serif", Font.PLAIN, 20));
-        player.add(t2, constraints);
+        JLabel profileLabel = new JLabel("PFP");
+        profileLabel.setFont(font);
 
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        JLabel t3 = new JLabel("test3");
-        t3.setFont(new Font("Serif", Font.PLAIN, 20));
-        player.add(t3, constraints);
+        JLabel namePanel = new JLabel(playerName);
+        namePanel.setFont(font);
 
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        constraints.gridwidth = 2;
-        JLabel t4 = new JLabel("test4");
-        t4.setFont(new Font("Serif", Font.PLAIN, 20));
-        player.add(t4, constraints);
+        JLabel pieceColorPanel = new JLabel("                     ");
+        pieceColorPanel.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(1.0f), Color.black));
+        pieceColorPanel.setBackground(pieceColor);
+        pieceColorPanel.setOpaque(true);
+
+        JLabel scorePanel = new JLabel("Score: " + playerScore);
+        scorePanel.setFont(font);
+
+        setConstraints(constraints, 0, 0, 0.0, 0.0);
+        player.add(profileLabel, constraints);
+
+        setConstraints(constraints, 1, 0, 1.0, 0.0);
+        player.add(namePanel, constraints);
+
+        setConstraints(constraints, 0, 1, 0.0, 1.0);
+        player.add(pieceColorPanel, constraints);
+
+        setConstraints(constraints, 1, 1, 1.0, 1.0);
+        player.add(scorePanel, constraints);
     }
 
+
+    public static void setConstraints(GridBagConstraints constraints, int gridX, int gridY, double weightX, double weightY) {
+        constraints.gridx = gridX;
+        constraints.gridy = gridY;
+        constraints.weightx = weightX;
+        constraints.weighty = weightY;
+    }
 
     public static void main(String[] args) {
         createTestField();
