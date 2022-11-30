1efaa833f0dfa719a48200c521149f822991e8cb
DominicVerschoor <dominic.verschoor@student.maastrichtuniversity.nl>
2022-11-30 16:46:59 +0100

    Calendar bracket completed

--- src/LA_BUNDES_PREMIER_LIG_MLS_ONE/Calendar.java
+++ src/LA_BUNDES_PREMIER_LIG_MLS_ONE/Calendar.java
@@ -1,29 +1,25 @@
 package LA_BUNDES_PREMIER_LIG_MLS_ONE;
 
 import PLAYER.*;
-import PLAYER.MTS.SELECTION_HEURISTICS.*;
-import PLAYER.P_BOT.*;
-import PLAYER.RULE_BASED_BOT.*;
 
 import java.util.ArrayList;
 
 public class Calendar {
 
-    public static ArrayList<ArrayList<Player>> getPermutation(Player[] playerList) {
-        ArrayList<ArrayList<Player>> result = new ArrayList<>();
+    public static ArrayList<ArrayList<Player>> getPermutation(Player[] whitePlayers, Player[] blackPlayers) {
+        ArrayList<ArrayList<Player>> pairing = new ArrayList<>();
 
-        for (int i = 0; i < playerList.length; i++) {
+        for (Player white : whitePlayers) {
 
-            for (int j = 0; j < playerList.length; j++) {
+            for (Player black : blackPlayers) {
 
-                if (i != j) {
-                    ArrayList<Player> tmp = new ArrayList<>();
-                    tmp.add(playerList[i]);
-                    tmp.add(playerList[j]);
-                    result.add(tmp);
-                }
+                ArrayList<Player> tmp = new ArrayList<>();
+                tmp.add(white);
+                tmp.add(black);
+                pairing.add(tmp);
+
             }
         }
-        return result;
+        return pairing;
     }
 }
